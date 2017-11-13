07ddbc62880f696b9449bc4ce7b472ada4784db1
jborbon <jborbon@live.com>
2017-11-13 15:48:11 -0500

    google api infobox

--- gp_final/app/controllers/restaurants_controller.rb
+++ gp_final/app/controllers/restaurants_controller.rb
@@ -136,6 +136,6 @@ class RestaurantsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def restaurant_params
-      params.require(:restaurant).permit(:name, :description, :phone_number, :address, :state, :city, :zipcode, :open_hours, :close_hours,:latitude,:longitude) 
+      params.require(:restaurant).permit(:name, :description, :phone_number, :address, :state, :city, :zipcode, :open_hours, :close_hours,:latitude,:longitude,:image ) 
     end
 end
--- gp_final/app/models/restaurant.rb
+++ gp_final/app/models/restaurant.rb
@@ -4,7 +4,8 @@ class Restaurant < ApplicationRecord
   has_many :menu_items
   has_many :users
 
-
+has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   def full_address
   	"#{address} #{city} #{state} #{zipcode}"
@@ -14,14 +15,22 @@ def self.search(search)
   where("name LIKE ? OR description LIKE ? OR zipcode LIKE ? OR category LIKE ? ", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%") 
 end
 
-def to_json
-        "{'id': #{id}, 'longitude': #{longitude}, 'latitude': #{latitude}}"
+    def to_json
+        "{'id': #{id}, 'longitude': #{longitude}, 'latitude': #{latitude}, 'name': #{name}}, 'address' : #{address}}, 'description' : #{description}, 'state' : #{state} , 'city' : #{city} , 'zipcode' : #{zipcode} , 'image' : #{image} "
     end
     def to_h
         {
             id: id,
             lng: longitude.to_f,
-            lat: latitude.to_f
+            lat: latitude.to_f,
+            name: name,
+            address: address,
+            description: description,
+            state: state,
+            city: city ,
+            zipcode: zipcode,
+            image: image 
+
         }
     end
 
